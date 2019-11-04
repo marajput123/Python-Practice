9b4c5dbed8176719de4bde0010bce689da3eef33
ahmed <muammadrajput99@gmail.com>
2019-11-04 18:29:55 -0500

    python Dictionary practice 11/4/2019 6:30 PM

---
+++ Dictionary.py
@@ -0,0 +1,34 @@
+# key={"google":{"charlie":["this@gmail.com", "lol@AOL.com", "biscuit@doll.com"], "Lola":["dod@dod.com", "lodo.gogo.com", "gactos@doli.com"]}}
+
+# for e in range(0, len(key[1])):
+#     for i in range(0, len(key[1][e])):
+#         # print(i)
+#         print(key[1][e][i])
+
+
+# a=list(key)
+# b=list(key["key1"])
+# c=list(key["key1"]["key1.1"])
+# print(a)
+# print(b)
+# print(c)
+# print(len(c))
+# a=list(key)
+# print(a)
+
+# diction={'k1':{'k1.1':{'k1.1.1':3},'k1.2':{'k1.3.1':1},'k1.3':{"k1.2.1":1}}}
+# # diction={'k1':1,"k2":2,'k3':3,'k4':4}
+# # print(diction['k1']['k1.1']['k1.1.1'])
+# for x, y in diction.items():
+#     print(x,y)
+
+
+# student={'name':'ahmed', 'age':'20', 'height':"5'10", 'country':'Pakistan'}
+# for i,y in student.items():
+#     print(i, y)
+
+
+
+        
+
+
